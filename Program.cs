2e315c7c3b7dad8c593205e9b4b70062761457eb
Matous Simunek <1165@student.itgymnazium.cz>
2023-12-14 11:21:16 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,118 @@
+﻿using System.Reflection.Metadata;
+using System.Reflection.Metadata.Ecma335;
+
+/*namespace _11653725_functions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TextLength();
+
+        }
+
+        static void TextLength()
+        {
+            string input1 = Console.ReadLine();
+            int Parameter = input1.Length;
+        }
+
+        public int product(int Parameter)
+        {
+            return Parameter;
+        }
+
+    }
+
+  
+}*/
+
+/*
+namespace _11653725_functions
+{
+    internal class Program2
+    {
+        static void Main(string[] args)
+        {
+            SquareRoot();
+
+        }
+
+        static void SquareRoot()
+        {
+            string input2 = Console.ReadLine();
+            int number = int.Parse(input2);
+
+            int Parameter2 = number * number;
+        }
+
+        public int product(int Parameter2)
+        {
+            return Parameter2;
+        }
+    }
+
+}*/
+
+/*
+namespace _11653725_functions
+{
+    internal class Program3
+    {
+        static void Main(string[] args)
+        {
+            PrintHappyBirthday();
+
+        }
+
+        static void PrintHappyBirthday()
+        {
+            string input3 = Console.ReadLine();
+            bool text = bool.Parse(input3);
+
+            if (text = false)
+            {
+                string Parameter3 = "";
+
+            } else if (text = true)
+            {
+                string Parameter3 = "Vše nejlepší!";
+            }
+        }
+
+        public string product(string Parameter3)
+        {
+            return Parameter3;
+        }
+
+    }
+
+
+}
+*/
+
+namespace _11653725_functions
+{
+    internal class Program4
+    {
+        static void Main(string[] args)
+        {
+            IsOdd();
+
+        }
+
+        static void IsOdd()
+        {
+            string input4 = Console.ReadLine();
+            int Parameter4 = input4.Length;
+        }
+
+        public int product(int Parameter4)
+        {
+            return Parameter4;
+        }
+
+    }
+
+
+}
